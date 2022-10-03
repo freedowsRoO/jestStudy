453047a01ac265c3efec979504049978385f223e
freedowsRoO <jefferson.fabrin@gmail.com>
2022-10-02 21:48:17 -0400

    initial commit

--- index.js
+++
@@ -1,5 +0,0 @@
-const somaHorasExtras = (salario, valorHorasExtra) => salario + valorHorasExtra;
-
-const calculaDesconto = (salario, desconto) => salario - desconto;
-
-export { somaHorasExtras, calculaDesconto };
